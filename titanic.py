b074fd45a1eacf89a4e3a91c50a8c82fa7563d74
william-barros-costa <william.de.barros.da.costa@gmail.com>
2023-02-28 17:10:54 +0000

    Start analysing data

--- titanic.py
+++ titanic.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %% Constants
-LOCATION_GENDER_SUBMISSION = r"C:\Users\William\Desktop\repository\kaggle_competitions\titanic\resources\gender_submission.csv"
 LOCATION_TRAIN = r"C:\Users\William\Desktop\repository\kaggle_competitions\titanic\resources\train.csv"
 LOCATION_TEST = r"C:\Users\William\Desktop\repository\kaggle_competitions\titanic\resources\test.csv"
 
@@ -12,14 +11,17 @@ LOCATION_TEST = r"C:\Users\William\Desktop\repository\kaggle_competitions\titani
 
 
 # %% Import data
-frame_gender_submission = pd.read_csv(LOCATION_GENDER_SUBMISSION)
 frame_train  = pd.read_csv(LOCATION_TRAIN)
 frame_test = pd.read_csv(LOCATION_TEST)
 
-frame_gender_submission.columns, frame_train.columns, frame_test.columns
+frame_train.columns, frame_test.columns
 # %% Basic Data Analysis
-print("Shape frame_gender_submission", frame_gender_submission.shape)
 print("Shape frame_train", frame_train.shape)
 print("Shape frame_test", frame_test.shape)
 
-# %%
+# %% Missing Data Analysis
+print("NaN count frame_train", frame_train.shape[0] - frame_train.count())
+print("NaN count frame_test", frame_test.shape[0] - frame_test.count())
+
+# %% Fill Empty values
+frame_train.fillna({"Cabin": "", "Age": -1})
